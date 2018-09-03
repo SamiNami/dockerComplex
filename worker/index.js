61e101ef58ec27419b48491062cb86ec2eb77218
sami peralahti <sami.peralahti@vainu.io>
2018-09-02 17:26:28 -0700

    setup for the dockerizing

--- worker/index.js
+++ worker/index.js
@@ -1,5 +1,5 @@
-const keys = require("./keys");
-const redis = require("redis");
+const keys = require('./keys');
+const redis = require('redis');
 
 const redisClient = redis.createClient({
   host: keys.redisHost,
@@ -9,17 +9,11 @@ const redisClient = redis.createClient({
 const sub = redisClient.duplicate();
 
 function fib(index) {
-  if (index < 2) {
-    return 1;
-  }
+  if (index < 2) return 1;
   return fib(index - 1) + fib(index - 2);
 }
 
-// when we get a new value that shows up in redis,
-// we are going to calulate a new fib value, and insrt it into a hash of values
-// index : value
-sub.on("message", (channel, message) => {
-  redisClient.hset("values", message, fib(parseInt(message)));
+sub.on('message', (channel, message) => {
+  redisClient.hset('values', message, fib(parseInt(message)));
 });
-
-sub.subscribe("insert");
+sub.subscribe('insert');
